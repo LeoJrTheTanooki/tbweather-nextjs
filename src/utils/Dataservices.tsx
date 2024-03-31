5741c2a64b52b0d8b6438f531100c755cfb46ced
LeoJrTheTanooki <leodevin4118@gmail.com>
2024-03-30 20:58:51 -0700

    Chaanged http to https

--- src/utils/Dataservices.tsx
+++ src/utils/Dataservices.tsx
@@ -5,13 +5,13 @@ const apiKey = process.env.NEXT_PUBLIC_API_KEY;
 
 export const getWeather = async (location: string = 'stockton') => {
   console.log(process.env.REACT_APP_API_KEY)
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=imperial`);
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${apiKey}&units=imperial`);
   const data: Iweather = await response.json();
   return data;
 };
 
 export const getForecast = async (location: string = 'stockton') => {
-  const response = await fetch(`http://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=imperial`);
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${location}&appid=${apiKey}&units=imperial`);
   const data: IForecast = await response.json();
   return data;
 };
